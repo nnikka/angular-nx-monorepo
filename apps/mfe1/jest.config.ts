47370cd61171bef609a15d437fbde555037a4f29
nnikka <nika.kurashvili@space.ge>
2024-04-29 15:44:36 +0400

    add apps

--- apps/mfe1/jest.config.ts
+++ apps/mfe1/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'monorepo',
+  displayName: 'mfe1',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/monorepo',
+  coverageDirectory: '../../coverage/apps/mfe1',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
--- apps/shell/src/app/app.component.spec.ts
+++ apps/shell/src/app/app.component.spec.ts
@@ -15,13 +15,13 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome monorepo'
+      'Welcome shell'
     );
   });
 
-  it(`should have as title 'monorepo'`, () => {
+  it(`should have as title 'shell'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('monorepo');
+    expect(app.title).toEqual('shell');
   });
 });
